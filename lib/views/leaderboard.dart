9632d683a28f0da3dfd29407523f43e151fee729
Hemanth5603 <shemanth2003.vskp@gmail.com>
2024-06-13 17:09:25 +0530

    leaderboard

--- lib/views/leaderboard.dart
+++ lib/views/leaderboard.dart
@@ -58,8 +58,8 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                             height: 30,
                           ),
                           Container(
-                            width: 200,
-                            height: 200,
+                            width: 150,
+                            height: 150,
                             decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(150),
                                 color: Color.fromARGB(255, 207, 233, 255),
@@ -71,15 +71,14 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                             child: Center(
                               child: Image.asset(
                                 "assets/icons/podium.png",
-                                width: 100,
+                                width: 70,
                               ),
                             ),
                           ),
                           Container(
                             width: MediaQuery.of(context).size.width,
-                            height: 212,
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 35),
+                            height: 185,
+                            margin: EdgeInsets.fromLTRB(20, 30, 20, 10),
                             decoration: BoxDecoration(
                                 color: Colors.white,
                                 border: Border.all(
@@ -96,26 +95,137 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
                                   height: 15,
                                 ),
                                 LeaderboardTile(
+                                  index: 0,
                                   icon: "assets/icons/first.png",
-                                  name: "Ishitha",
-                                  contributions: "26",
+                                  name: dataController.leaderboardModel
+                                          .leaderboard![0].name ??
+                                      "User 1",
+                                  contributions: dataController.leaderboardModel
+                                      .leaderboard![0].contributions
+                                      .toString(),
                                   last: false,
+                                  isIcon: true,
                                 ),
                                 LeaderboardTile(
+                                  index: 0,
                                   icon: "assets/icons/second.png",
-                                  name: "Hemanth",
-                                  contributions: "26",
+                                  name: dataController.leaderboardModel
+                                          .leaderboard![1].name ??
+                                      "User 2",
+                                  contributions: dataController.leaderboardModel
+                                      .leaderboard![1].contributions
+                                      .toString(),
                                   last: false,
+                                  isIcon: true,
                                 ),
                                 LeaderboardTile(
+                                  index: 0,
                                   icon: "assets/icons/third.png",
-                                  name: "Ishitha",
-                                  contributions: "26",
+                                  name: dataController.leaderboardModel
+                                          .leaderboard![2].name ??
+                                      "User 3",
+                                  contributions: dataController.leaderboardModel
+                                      .leaderboard![2].contributions
+                                      .toString(),
                                   last: true,
+                                  isIcon: true,
                                 )
                               ],
                             ),
-                          )
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 60,
+                            padding: EdgeInsets.all(15),
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: AppConstants.customBlue),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  "Rank",
+                                  style: TextStyle(
+                                      fontFamily: 'man-r',
+                                      fontSize: 14,
+                                      color: Colors.white),
+                                ),
+                                Container(
+                                  width: 1,
+                                  height: 10,
+                                  color:
+                                      const Color.fromARGB(195, 255, 255, 255),
+                                ),
+                                Text(
+                                  "Name",
+                                  style: TextStyle(
+                                      fontFamily: 'man-r',
+                                      fontSize: 14,
+                                      color: Colors.white),
+                                ),
+                                Container(
+                                  width: 1,
+                                  height: 10,
+                                  color:
+                                      const Color.fromARGB(195, 255, 255, 255),
+                                ),
+                                Text(
+                                  "Contributions",
+                                  style: TextStyle(
+                                      fontFamily: 'man-r',
+                                      fontSize: 14,
+                                      color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 255,
+                              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                      color:
+                                          Color.fromARGB(190, 221, 221, 221)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color:
+                                            Color.fromARGB(200, 240, 248, 255),
+                                        blurRadius: 15)
+                                  ],
+                                  borderRadius: BorderRadius.circular(15)),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 15.0),
+                                child: ListView.builder(
+                                    itemCount: dataController.leaderboardModel
+                                            .leaderboard!.length -
+                                        3,
+                                    itemBuilder: (context, index) {
+                                      return LeaderboardTile(
+                                        index: index,
+                                        contributions: dataController
+                                            .leaderboardModel
+                                            .leaderboard![index + 3]
+                                            .contributions
+                                            .toString(),
+                                        icon: "assets/icons/bronze.png",
+                                        name: dataController.leaderboardModel
+                                            .leaderboard![index + 3].name
+                                            .toString(),
+                                        last: dataController.leaderboardModel
+                                                        .leaderboard!.length -
+                                                    1 ==
+                                                index
+                                            ? true
+                                            : false,
+                                        isIcon: false,
+                                      );
+                                    }),
+                              )),
                         ],
                       ),
                     ),
@@ -126,21 +236,25 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
 }
 
 class LeaderboardTile extends StatelessWidget {
+  int index;
   String icon;
   String name;
   String contributions;
   bool last;
+  bool isIcon;
   LeaderboardTile(
       {super.key,
+      required this.index,
       required this.contributions,
       required this.icon,
       required this.name,
-      required this.last});
+      required this.last,
+      required this.isIcon});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 65,
+      height: 55,
       padding: EdgeInsets.symmetric(vertical: 00),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -152,30 +266,34 @@ class LeaderboardTile extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   SizedBox(
-                    width: 10,
+                    width: 20,
                   ),
-                  Padding(
-                      padding: const EdgeInsets.all(0.0),
-                      child: Image.asset(
-                        icon,
-                        width: 40,
-                      )),
+                  isIcon == true
+                      ? Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Image.asset(
+                            icon,
+                            width: 30,
+                          ))
+                      : Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Text((index + 4).toString())),
                   SizedBox(
-                    width: 10,
+                    width: 15,
                   ),
                   Text(
                     name,
                     style: TextStyle(
-                        fontSize: 14,
+                        fontSize: 12,
                         fontFamily: 'man-r',
                         fontWeight: FontWeight.w400),
                   ),
                 ],
               ),
               Container(
-                width: 30,
-                height: 30,
-                margin: EdgeInsets.only(right: 15),
+                width: 25,
+                height: 25,
+                margin: EdgeInsets.only(right: 20),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(50),
                     color: const Color.fromARGB(255, 184, 223, 255)),
@@ -190,7 +308,7 @@ class LeaderboardTile extends StatelessWidget {
           if (last == false)
             Container(
               margin: EdgeInsets.symmetric(vertical: 8),
-              width: MediaQuery.of(context).size.width - 50,
+              width: MediaQuery.of(context).size.width,
               height: 1,
               color: Color.fromARGB(132, 206, 206, 206),
             )
